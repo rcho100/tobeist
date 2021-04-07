59fb0c0b77addeb0b64fd4906532fabcdc661858
Richard Cho <richardcho100@gmail.com>
2021-04-07 16:38:14 -0400

    Add top section with icons for sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
 
 function Sidebar() {
   return (
     <div className="sidebar" data-testid="sidebar">
-      <p>Sidebar</p>
+      <ul className="sidebar__top">
+        <li>
+          <span><FaInbox /></span>
+          <span> Inbox</span>
+        </li>
+        <li>
+          <span><FaRegCalendar /></span>
+          <span> Today</span>
+        </li>
+        <li>
+          <span><FaRegCalendarAlt /></span>
+          <span> Upcoming</span>
+        </li>
+      </ul>
     </div>
   );
 }
